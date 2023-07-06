b5f93598ef871de04b25283a4c5990786571c984
Teo <teoirado@gmail.com>
2023-07-06 17:42:36 -0300

    feat: Criando formulário, create e edit

---
+++ src/types/MedicalRecord.ts
@@ -0,0 +1,18 @@
+export type MedicalRecordProps = {
+  id?: number;
+  fullName: string;
+  cpf: string;
+  phoneNumber: string;
+  photo: File;
+  photoPath: string;
+  address?: Address;
+  userId?: number;
+};
+
+export type Address = {
+  street: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  postalCode: string;
+};
